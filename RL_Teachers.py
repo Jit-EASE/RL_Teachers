b374c8993abdca57509173e4f7a6c540b0310da5
Shubhojit Bagchi <75479624+Jit-EASE@users.noreply.github.com>
2025-11-06 00:15:03 +0000

    Update RL_Teachers.py

--- RL_Teachers.py
+++ RL_Teachers.py
@@ -649,8 +649,8 @@ st.set_page_config(
 st.title("RL Teacher for Agentic Econometric & Policy AI — Irish Agrifood Systems")
 
 st.markdown(
-    "Designed and Developed by **Shubhojit Bagchi** \n"
-    "This prototype uses **Copernicus + econometric panel for Ireland (2016–2024)** "
+    "Designed and Developed by **Shubhojit Bagchi**"
+    "- This prototype uses **Copernicus + econometric panel for Ireland (2016–2024)** "
     "to simulate how a **Reinforcement Learning Teacher** can train an **Agentic AI "
     "Student** to choose econometric models and reason about agrifood policy trade-offs.\n\n"
     "- **Teacher**: decides which agrifood learning module and difficulty level to present  \n"
